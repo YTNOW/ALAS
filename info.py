89ae516391248ef9b8ff37d7593a475fcd1d44a0
ALAS <139692597+YTNOW@users.noreply.github.com>
2023-10-06 18:32:45 +0530

    Update info.py

--- info.py
+++ info.py
@@ -53,7 +53,7 @@ DATABASE_NAME = environ.get('DATABASE_NAME', "vkbotsthalpathy")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
-VERIFY = bool(environ.get('VERIFY', True))
+VERIFY = bool(environ.get('VERIFY', False))
 SHORTLINK_URL = environ.get('SHORTLINK_URL', 'exe.io')
 SHORTLINK_API = environ.get('SHORTLINK_API', '99bd06b1570e3998d287d08bcb8c00f0d6e0ffa0')
 IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', False))
